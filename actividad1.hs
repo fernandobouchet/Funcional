fec2ee0f6f8a3a8b2667195e975a9754e3751938
fernandobouchet <fernandobouchet@gmail.com>
2025-04-21 20:47:51 -0300

    actividad clase 1

---
+++ actividad1.hs
@@ -0,0 +1,10 @@
+suma numero1 numero2 = numero1 + numero2
+doble numero = numero * 2
+
+producto numero1 numero2 = numero1 * numero2
+
+cuadrado numero = numero ^ 2
+
+norma numero1 numero2 = sqrt (cuadrado numero1 + cuadrado numero2)
+
+constante8 numeero = 8
